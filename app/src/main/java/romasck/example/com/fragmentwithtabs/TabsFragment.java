b1ac4cfd5e8f5aaa1997ba4c3cf9ebb775a07046
roma.sck <roma.sck@gmail.com>
2016-12-10 23:19:30 +0200

    added tabs with two fragments/ simple functionality

--- app/src/main/java/romasck/example/com/fragmentwithtabs/TabsFragment.java
+++ app/src/main/java/romasck/example/com/fragmentwithtabs/TabsFragment.java
@@ -3,16 +3,18 @@ package romasck.example.com.fragmentwithtabs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TabsFragment extends Fragment {
-
+    private FragmentTabHost mTabHost;
 
     public TabsFragment() {
         // Required empty public constructor
@@ -23,7 +25,18 @@ public class TabsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tabs, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_tabs,container, false);
+
+
+        mTabHost = (FragmentTabHost)rootView.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.tabs_content);
+
+        TabHost.TabSpec tab1 = mTabHost.newTabSpec("tab1").setIndicator("Fragment 1");
+        TabHost.TabSpec tab2 = mTabHost.newTabSpec("tab2").setIndicator("Fragment 2");
+        mTabHost.addTab(tab1, FragmentOne.class, null);
+        mTabHost.addTab(tab2, FragmentTwo.class, null);
+
+        return rootView;
     }
 
 }
