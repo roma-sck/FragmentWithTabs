e764800921db883cd74d0d83a41b66ab47b391ed
roma.sck <roma.sck@gmail.com>
2016-12-18 02:09:31 +0200

    tabhost changed to viewpager fir sliding tabs

--- app/src/main/java/romasck/example/com/fragmentwithtabs/TabsFragment.java
+++ app/src/main/java/romasck/example/com/fragmentwithtabs/TabsFragment.java
@@ -1,23 +1,22 @@
 package romasck.example.com.fragmentwithtabs;
 
-
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TabHost;
 import android.widget.TextView;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TabsFragment extends Fragment {
-    private FragmentTabHost mTabHost;
+    ViewPager pager;
+    TabsFragmentAdapter adapter;
+    TabLayout tabLayout;
 
     public TabsFragment() {
         // Required empty public constructor
@@ -28,43 +27,38 @@ public class TabsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_tabs, container, false);
+        pager = (ViewPager) rootView.findViewById(R.id.chat_view_pager);
+        adapter = new TabsFragmentAdapter(getChildFragmentManager());
+        adapter.addFragment(new FragmentOne(), getString(R.string.tab_chat_room_title));
+        adapter.addFragment(new FragmentTwo(), getString(R.string.tab_chat_user_list_title));
+        pager.setAdapter(adapter);
 
-        mTabHost = (FragmentTabHost) rootView.findViewById(android.R.id.tabhost);
-        mTabHost.setup(getActivity(), getActivity().getSupportFragmentManager(), android.R.id.tabcontent);
-
-        TabHost.TabSpec tab1 = mTabHost
-                .newTabSpec("tab1")
-                .setIndicator(
-                        getTabIndicator(mTabHost.getContext(),
-                                R.string.tab_chat_room_title,
-                                R.drawable.ic_chat_room_selector)
-                );
-        TabHost.TabSpec tab2 = mTabHost
-                .newTabSpec("tab2")
-                .setIndicator(
-                        getTabIndicator(mTabHost.getContext(),
-                                R.string.tab_chat_user_list_title,
-                                R.drawable.ic_user_list_selector)
-                );
-
-        mTabHost.addTab(tab1, FragmentOne.class, null);
-        mTabHost.addTab(tab2, FragmentTwo.class, null);
+        tabLayout = (TabLayout) getActivity().findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.getTabAt(0).setCustomView(getTabIndicator(
+                getActivity(), R.string.tab_chat_room_title, R.drawable.ic_chat_room_selector));
+        tabLayout.getTabAt(1).setCustomView(getTabIndicator(
+                getActivity(), R.string.tab_chat_user_list_title, R.drawable.ic_user_list_selector));
 
         return rootView;
     }
 
     private View getTabIndicator(Context context, int title, int icon) {
-        View view = LayoutInflater.from(context).inflate(R.layout.tab_header, null);
-        ImageView iv = (ImageView) view.findViewById(R.id.tab_icon_image);
-        iv.setImageResource(icon);
-        TextView tv = (TextView) view.findViewById(R.id.tab_title_text);
-        tv.setText(title);
+        TextView view = (TextView) LayoutInflater.from(context).inflate(R.layout.tab_header, null);
+        view.setText(title);
+        view.setCompoundDrawablesWithIntrinsicBounds(0, icon, 0, 0);
         return view;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mTabHost = null;
+    public void onResume() {
+        super.onResume();
+        tabLayout.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        tabLayout.setVisibility(View.GONE);
     }
 }
---
+++ app/src/main/java/romasck/example/com/fragmentwithtabs/TabsFragmentAdapter.java
@@ -0,0 +1,41 @@
+package romasck.example.com.fragmentwithtabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Roman Fomenko on 15.12.16.
+ */
+
+public class TabsFragmentAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public TabsFragmentAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
